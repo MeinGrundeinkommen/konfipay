e9b0f490a9b1fd6f650ffdfbe077ded296791fc8
Martin Tepper <mgpalmer@monogreen.de>
2021-10-19 15:21:13 +0200

    Implement error handling, query params, and parsing based on content type

--- lib/konfipay/client.rb
+++ lib/konfipay/client.rb
@@ -11,13 +11,11 @@ module Konfipay
       @bearer_token = nil
     end
 
-    def new_statements
+    def new_statements(params = {})
       authenticate if @bearer_token.nil?
       # TODO: Catch and retry 401 error
-      response = http.auth("Bearer #{@bearer_token}").get("#{@config.base_url}/api/v4/Document/Camt")
-      json = JSON.parse(response.body.to_s)
-      # TODO: Only get each statement doc if iban given and matching
-      # TODO: Get each new document, collect and parse each CAMT and get out the actual statement info
+      response = http.auth("Bearer #{@bearer_token}").get("#{@config.base_url}/api/v4/Document/Camt#{query_params(params)}")
+      raise_error_or_parse!(response)
     end
 
     def camt_file(r_id)
@@ -30,6 +28,40 @@ module Konfipay
       HTTP.timeout(@config.timeout)
         .headers(accept: "application/json")
         .use(logging: { logger: @config.logger })
+    end
+
+    def query_params(params)
+      if params.any?
+        "?" + params.to_query
+      else
+        ""
+      end
+    end
+
+    def raise_error_or_parse!(response)
+      case response.status
+      when 200
+        parse(response)
+      # TODO: Create error classes for common errors
+      when 400
+        # {"errorItems":[{"errorCode":"ERR-04-0009","errorMessage":"UnknownBankAccount","timestamp":"2021-10-19T15:10:45.767"}]}
+        errors = parse(response)["errorItems"].map { |e| e["errorMessage"] }.join(", ")
+        raise "400 Bad Request, errors: #{errors}" 
+      when 404
+        # {"Message":"Welcome to konfipay. There is no API-Endpoint defined for 'https://portal.konfipay.de/api/v4/Document/Camtiban=aaaa'. Please take a look at the konfipay API-Documentation for valid API-Endpoints","ApiDocumentationLink":"https://portal.konfipay.de/Info/Api_Doc"}
+        raise "404 Error: #{parse(response)['Message'].inspect}"
+      else
+        raise "Unhandled HTTP response code: #{response.status.inspect}: \"#{response.body.to_s}\""
+      end
+    end
+
+    def parse(response)
+      case response.content_type.mime_type
+      when "application/json"
+        JSON.parse(response.body.to_s)
+      else
+        raise "Unknown content_type #{response.content_type.inspect}!"
+      end
     end
 
     def authenticate
--- lib/konfipay/operations/fetch_statements.rb
+++ lib/konfipay/operations/fetch_statements.rb
@@ -2,12 +2,21 @@ module Konfipay
   module Operations
     class FetchStatements < Base
       def fetch(which_ones, filters = {})
-        case which_ones
+        iban = filters["iban"].presence
+        opts = {}
+        opts["iban"] = iban if iban
+        list = case which_ones
         when "new"
-          ["bli", "bla"]
+          @client.new_statements(opts)
         else
           raise "not implemented yet"
         end
+
+        binding.pry
+
+      # TODO: Only get each statement doc if iban given and matching
+      # TODO: Get each new document, collect and parse each CAMT and get out the actual statement info
+
       end
     end
   end
