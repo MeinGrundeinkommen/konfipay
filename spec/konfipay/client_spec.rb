7c566a708e5ccd81dd71722d54830e091534deee
Martin Tepper <mgpalmer@monogreen.de>
2021-11-02 16:27:27 +0100

    Spec cases for providing arguments to be turned into query params, refactor spec, and add case for 204 response

--- spec/konfipay/client_spec.rb
+++ spec/konfipay/client_spec.rb
@@ -3,7 +3,8 @@
 require 'spec_helper'
 # rubocop:disable Metrics/BlockLength
 # rubocop:disable Metrics/MethodLength
-# rubocop:disable RSpec/ExampleLength
+# rubocop:disable RSpec/MultipleMemoizedHelpers
+# rubocop:disable RSpec/NestedGroups
 RSpec.describe Konfipay::Client do
   let(:api_key) { "le key d'api" }
   let(:access_token) { 'xyz123' }
@@ -11,9 +12,19 @@ RSpec.describe Konfipay::Client do
   let!(:config) do
     Konfipay.configure do |c|
       c.api_key = api_key
-      # Uncomment this for debug output during specs
+      # Uncomment this for debug output from http gem during specs
       # c.logger = Logger.new($stdout)
     end
+  end
+
+  let(:request_headers) do
+    {
+      'Accept' => 'application/json',
+      'Authorization' => "Bearer #{access_token}",
+      'Connection' => 'close',
+      'Host' => 'portal.konfipay.de',
+      'User-Agent' => 'http.rb/5.0.4'
+    }
   end
 
   def stub_login_token_api_call!
@@ -44,22 +55,38 @@ RSpec.describe Konfipay::Client do
   end
 
   describe 'new_statements' do
+    let(:stubbed_url) { 'https://portal.konfipay.de/api/v4/Document/Camt' }
+    let(:result) { described_class.new.new_statements }
+
+    context 'when konfipay returns no content' do
+      before do
+        stub_login_token_api_call!
+        stub_request(:get, stubbed_url)
+          .with(headers: request_headers)
+          .to_return(status: 204)
+      end
+
+      it 'returns nil' do
+        expect(result).to eq(nil)
+      end
+    end
+
     context 'when konfipay returns success' do
-      let(:successful_response) { described_class.new.new_statements(params) }
-      let(:params) { {} }
+      let(:expected_parsed_json) do
+        { 'documentItems' =>
+          [{ 'rId' => '5c19b66h-3d6e-4e8a-4548-622bd50a7af2',
+             'href' => 'api/v4.0/Document/Camt/5c19b66h-3d6e-4e8a-4548-622bd50a7af2',
+             'timestamp' => '2021-10-28T23:21:59+02:00',
+             'iban' => 'DE02300606010002474689',
+             'isNew' => true,
+             'format' => 'camt.053',
+             'fileName' => '2021-10-28_C53_DE02300606010002474689_EUR_365352.xml' }] }
+      end
 
       before do
         stub_login_token_api_call!
-        stub_request(:get, 'https://portal.konfipay.de/api/v4/Document/Camt')
-          .with(
-            headers: {
-              'Accept' => 'application/json',
-              'Authorization' => "Bearer #{access_token}",
-              'Connection' => 'close',
-              'Host' => 'portal.konfipay.de',
-              'User-Agent' => 'http.rb/5.0.4'
-            }
-          )
+        stub_request(:get, stubbed_url)
+          .with(headers: request_headers)
           .to_return(status: 200,
                      body: {
                        documentItems: [
@@ -77,21 +104,27 @@ RSpec.describe Konfipay::Client do
                      headers: { 'Content-Type' => 'application/json; charset=UTF-8' })
       end
 
-      it 'returns list of new documents' do
-        expect(successful_response).to eq(
-          { 'documentItems' =>
-            [{ 'rId' => '5c19b66h-3d6e-4e8a-4548-622bd50a7af2',
-               'href' => 'api/v4.0/Document/Camt/5c19b66h-3d6e-4e8a-4548-622bd50a7af2',
-               'timestamp' => '2021-10-28T23:21:59+02:00',
-               'iban' => 'DE02300606010002474689',
-               'isNew' => true,
-               'format' => 'camt.053',
-               'fileName' => '2021-10-28_C53_DE02300606010002474689_EUR_365352.xml' }] }
-        )
+      context 'without arguments' do
+        it 'returns list of new documents' do
+          expect(result).to eq(expected_parsed_json)
+        end
+      end
+
+      context 'with iban filter argument' do
+        let(:stubbed_url) { 'https://portal.konfipay.de/api/v4/Document/Camt?iban=an%20iban%20maybe' }
+        let(:result) { described_class.new.new_statements('iban' => 'an iban maybe') }
+
+        it 'returns list of new documents' do
+          expect(result).to eq(expected_parsed_json)
+        end
       end
     end
+  end
+
+  describe 'camt_file' do
   end
 end
 # rubocop:enable Metrics/BlockLength
 # rubocop:enable Metrics/MethodLength
-# rubocop:enable RSpec/ExampleLength
+# rubocop:enable RSpec/MultipleMemoizedHelpers
+# rubocop:enable RSpec/NestedGroups
