e9e7217fd42c6fb066296225b72e67be6d37b879
Martin Tepper <mgpalmer@monogreen.de>
2022-04-06 13:17:05 +0200

    Allow Operations to be run in a specific sidekiq queue (#26)
    * Let's try a separate queue
    
    * Make it possible to specify a specific sidekiq queue when using the top-level methods.
    Update gem dependencies while we're at it.
    
    * Update version since the job queue is a breaking change
    
    * Bump version
    
    * Trying to force newer nokogiri version as there is a security warning
    
    * This doesn't work

--- lib/konfipay/version.rb
+++ lib/konfipay/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module Konfipay
-  VERSION = '1.3.2'
+  VERSION = '1.4.1'
 end
--- spec/konfipay_spec.rb
+++ spec/konfipay_spec.rb
@@ -10,42 +10,66 @@ RSpec.describe Konfipay do
   describe 'class methods' do
     let(:callback_class) { 'ExampleCallbackClass' }
     let(:callback_method) { 'example_callback_class_method' }
+    let(:queue) { nil }
     let(:iban) { 'DE02120300000000202051' }
+    let(:sidekiq_options_dummy) { Class.new }
 
     describe 'new_statements' do
       subject { request_fetch }
 
       let(:request_fetch) do
-        described_class.new_statements(callback_class, callback_method, iban, mark_as_read)
+        described_class.new_statements(callback_class, callback_method, queue, iban, mark_as_read)
       end
+
       let(:mark_as_read) { false }
 
       before do
-        allow(Konfipay::Jobs::FetchStatements).to receive(:perform_async)
+        allow(sidekiq_options_dummy).to receive(:perform_async)
+        allow(Konfipay::Jobs::FetchStatements).to receive(:set).and_return(sidekiq_options_dummy)
       end
 
       it { is_expected.to be(true) }
 
-      it 'enqueues a job with passed-in arguments' do
-        request_fetch
-        expect(Konfipay::Jobs::FetchStatements).to have_received(:perform_async).with(
-          callback_class,
-          callback_method,
-          'new',
-          { 'iban' => iban },
-          { 'mark_as_read' => mark_as_read }
-        )
+      context 'with passed-in arguments' do
+        let(:queue) { :fast }
+
+        it 'enqueues a job' do
+          request_fetch
+          expect(sidekiq_options_dummy).to have_received(:perform_async).with(
+            callback_class,
+            callback_method,
+            'new',
+            { 'iban' => iban },
+            { 'mark_as_read' => mark_as_read }
+          )
+        end
+
+        it 'uses the named queue' do
+          request_fetch
+          expect(Konfipay::Jobs::FetchStatements).to have_received(:set).with(queue: queue)
+        end
       end
 
-      it 'enqueues a job with default arguments' do
-        described_class.new_statements(callback_class, callback_method)
-        expect(Konfipay::Jobs::FetchStatements).to have_received(:perform_async).with(
-          callback_class,
-          callback_method,
-          'new',
-          { 'iban' => nil },
-          { 'mark_as_read' => true }
-        )
+      context 'with default arguments' do
+        let(:request_fetch) do
+          described_class.new_statements(callback_class, callback_method)
+        end
+
+        it 'enqueues a job' do
+          request_fetch
+          expect(sidekiq_options_dummy).to have_received(:perform_async).with(
+            callback_class,
+            callback_method,
+            'new',
+            { 'iban' => nil },
+            { 'mark_as_read' => true }
+          )
+        end
+
+        it 'uses the default queue' do
+          request_fetch
+          expect(Konfipay::Jobs::FetchStatements).to have_received(:set).with(queue: :default)
+        end
       end
 
       # TODO: check arguments are checked
@@ -55,37 +79,59 @@ RSpec.describe Konfipay do
       subject { request_fetch }
 
       let(:request_fetch) do
-        described_class.statement_history(callback_class, callback_method, iban, from, to)
+        described_class.statement_history(callback_class, callback_method, queue, iban, from, to)
       end
+
       let(:from) { (Date.today - 100).iso8601 }
       let(:to) { (Date.today - 10).iso8601 }
 
       before do
-        allow(Konfipay::Jobs::FetchStatements).to receive(:perform_async)
+        allow(sidekiq_options_dummy).to receive(:perform_async)
+        allow(Konfipay::Jobs::FetchStatements).to receive(:set).and_return(sidekiq_options_dummy)
       end
 
       it { is_expected.to be(true) }
 
-      it 'enqueues a job with passed-in arguments' do
-        request_fetch
-        expect(Konfipay::Jobs::FetchStatements).to have_received(:perform_async).with(
-          callback_class,
-          callback_method,
-          'history',
-          { 'from' => from, 'iban' => iban, 'to' => to },
-          {}
-        )
+      context 'with passed-in arguments' do
+        let(:queue) { :fast }
+
+        it 'enqueues a job' do
+          request_fetch
+          expect(sidekiq_options_dummy).to have_received(:perform_async).with(
+            callback_class,
+            callback_method,
+            'history',
+            { 'from' => from, 'iban' => iban, 'to' => to },
+            {}
+          )
+        end
+
+        it 'uses the named queue' do
+          request_fetch
+          expect(Konfipay::Jobs::FetchStatements).to have_received(:set).with(queue: queue)
+        end
       end
 
-      it 'enqueues a job with default arguments' do
-        described_class.statement_history(callback_class, callback_method)
-        expect(Konfipay::Jobs::FetchStatements).to have_received(:perform_async).with(
-          callback_class,
-          callback_method,
-          'history',
-          { 'from' => Date.today.iso8601, 'iban' => nil, 'to' => Date.today.iso8601 },
-          {}
-        )
+      context 'with default arguments' do
+        let(:request_fetch) do
+          described_class.statement_history(callback_class, callback_method)
+        end
+
+        it 'enqueues a job' do
+          request_fetch
+          expect(sidekiq_options_dummy).to have_received(:perform_async).with(
+            callback_class,
+            callback_method,
+            'history',
+            { 'from' => Date.today.iso8601, 'iban' => nil, 'to' => Date.today.iso8601 },
+            {}
+          )
+        end
+
+        it 'uses the default queue' do
+          request_fetch
+          expect(Konfipay::Jobs::FetchStatements).to have_received(:set).with(queue: :default)
+        end
       end
 
       # TODO: check arguments are checked
