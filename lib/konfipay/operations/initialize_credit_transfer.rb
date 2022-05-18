e19ef95c039479a0b37f6766bbba42e8fc23bd90
Martin Tepper <mgpalmer@monogreen.de>
2022-05-18 16:48:44 +0200

    Move the xml creation into its own file/class, make it add the transaction id

--- lib/konfipay/operations/initialize_credit_transfer.rb
+++ lib/konfipay/operations/initialize_credit_transfer.rb
@@ -117,66 +117,7 @@ module Konfipay
         # TODO: validate payment data again?
 
         # client = Konfipay::Client.new
-
-        # TODO: Add possibility to split into batches right away?
-
-        # Comments here are from sepa_king docs: https://github.com/salesking/sepa_king
-        builder = SEPA::CreditTransfer.new(
-          # Name of the initiating party and debtor, in German: "Auftraggeber"
-          # String, max. 70 char
-          name: payment_data["debtor"]["name"],
-          # OPTIONAL: Business Identifier Code (SWIFT-Code) of the debtor
-          # String, 8 or 11 char
-          bic:  payment_data["debtor"]["bic"],
-          # International Bank Account Number of the debtor
-          # String, max. 34 chars
-          iban: payment_data["debtor"]["iban"]
-        )
-        payment_data["creditors"].each do |creditor_data|
-          builder.add_transaction(
-            # Name of the creditor, in German: "Zahlungsempfänger"
-            # String, max. 70 char
-            name:                   creditor_data["name"],
-            # OPTIONAL: Business Identifier Code (SWIFT-Code) of the creditor's account
-            # String, 8 or 11 char
-            bic:                    creditor_data["bic"],
-            # International Bank Account Number of the creditor's account
-            # String, max. 34 chars
-            iban:                   creditor_data["iban"],
-            # Amount
-            # Number with two decimal digit
-            amount:                 (Float(creditor_data["amount_in_cents"]) / 100),
-            # OPTIONAL: Currency, EUR by default (ISO 4217 standard)
-            # String, 3 char
-            currency:               creditor_data["currency"],
-            # OPTIONAL: End-To-End-Identification, will be submitted to the creditor
-            # String, max. 35 char
-            reference:              creditor_data["end_to_end_reference"],
-            # OPTIONAL: Unstructured remittance information, in German "Verwendungszweck"
-            # String, max. 140 char
-            remittance_information: creditor_data["remittance_information"],
-            # OPTIONAL: Requested execution date, in German "Ausführungstermin"
-            # Date
-            requested_date: Date.parse(creditor_data["execute_on"]),
-            # OPTIONAL: Enables or disables batch booking, in German "Sammelbuchung / Einzelbuchung"
-            # True or False
-            batch_booking: true,
-            # OPTIONAL: Urgent Payment
-            # One of these strings:
-            #   'SEPA' ("SEPA-Zahlung")
-            #   'URGP' ("Taggleiche Eilüberweisung")
-            service_level: 'SEPA'
-          )
-        end
-
-        xml = builder.to_xml("pain.001.003.03")
-        puts xml
-        binding.pry
-
-        # TODO: check data
-        ## pp data
-        # maybe split up in sub-tasks if there are very many transfers
-        # make PAIN xml in memory
+        xml = Konfipay::PainBuilder.new(payment_data, transaction_id).credit_transfer_xml # here comes the pain
         # upload to konfipay
         # parse/check result
 
