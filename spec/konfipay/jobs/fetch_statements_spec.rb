0fbc39e8784f5d2fd7252e0a2d2975a1084673f6
Martin Tepper <mgpalmer@monogreen.de>
2023-06-03 12:47:50 +0200

    Adapt spec, make sure configuration is instantiated with correct options from job params

--- spec/konfipay/jobs/fetch_statements_spec.rb
+++ spec/konfipay/jobs/fetch_statements_spec.rb
@@ -3,22 +3,36 @@
 require 'spec_helper'
 
 RSpec.describe Konfipay::Jobs::FetchStatements do
+  before do
+    Konfipay.configure do |config|
+      config.api_keys = {
+        'default' => '1',
+        'other' => '2'
+      }
+    end
+  end
+
+  after do
+    Konfipay::Configuration.initializer_block = nil
+  end
+
   describe 'perform' do
     let(:do_it) do
-      described_class.new(config).perform(
+      described_class.new.perform(
         'ExampleCallbackClass',
         'example_callback_fetch_statements',
         'new',
         { 'bla' => 'blub' },
-        { 'ladi' => 'da' }
+        { 'ladi' => 'da' },
+        { 'api_key_name' => 'other' }
       )
     end
 
     let(:data) { 'le_data' }
-    let(:config) { Konfipay.configuration(api_key: '<key>') }
-    let(:operation) { Konfipay::Operations::FetchStatements.new(config) }
+    let(:operation) { Konfipay::Operations::FetchStatements.new }
 
     before do
+      allow(Konfipay).to receive(:configuration).and_call_original
       allow(Konfipay::Operations::FetchStatements).to receive(:new).and_return(operation)
       allow(operation).to receive(:fetch).with(
         'new',
@@ -31,6 +45,16 @@ RSpec.describe Konfipay::Jobs::FetchStatements do
     it 'calls the operation' do
       do_it
       expect(operation).to have_received(:fetch)
+    end
+
+    it 'instantiates a new config' do
+      do_it
+      expect(Konfipay::Operations::FetchStatements).to have_received(:new).with(a_kind_of(Konfipay::Configuration))
+    end
+
+    it 'passes the config the config options' do
+      do_it
+      expect(Konfipay).to have_received(:configuration).with(api_key_name: 'other')
     end
   end
 end
